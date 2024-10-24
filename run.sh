ad7d37673e56d6c141fb2ffd0c59a981946e7333
gowdasagar06 <92599815+gowdasagar06@users.noreply.github.com>
2024-10-24 11:36:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [[ $run_sonar == "YES" ]]; then
     -Dsonar.projectKey=demo \
     -Dsonar.projectName='demo' \
     -Dsonar.host.url=http://13.235.100.209:9000 \
-    -Dsonar.token=squ_2f217b09857d7c2af399d2b22f807de615667451
+    -Dsonar.token=squ_2f217b09857d7c2af399d2b22f807de615667451   
 else
   echo "running maven without sonarqube"  
   # mvn clean verify
